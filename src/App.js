5456900fe38e4ea4fb06e0e48cabbf6f9677cc8a
hamzaali9590 <hamzaali9590@hotmail.com>
2024-09-02 21:52:42 +0500

    swapy related changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react'
+import './index.css'
+import { createSwapy } from 'swapy'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const DEFAULT = {
+  '1': 'a',
+  '3': 'c',
+  '4': 'd',
+  '2': null
 }
 
-export default App;
+
+function A() {
+  return (
+    <div className="item a" data-swapy-item="a">
+        <div className="handle" data-swapy-handle></div>
+        <div>A</div>
+    </div>
+  )
+}
+
+function C() {
+  return (
+    <div className="item c" data-swapy-item="c">
+        <div>C</div>
+    </div>
+  )
+}
+
+function D() {
+  return (
+    <div className="item d" data-swapy-item="d">
+        <div>D</div>
+    </div>
+  )
+}
+
+function getItemById(itemId) {
+  switch (itemId) {
+    case 'a':
+      return <A />
+    case 'c':
+      return <C />
+    case 'd':
+      return <D />
+  }
+}
+
+
+function App() {
+  const slotItems = localStorage.getItem('slotItem') ? JSON.parse(localStorage.getItem('slotItem')) : DEFAULT
+  useEffect(() => {
+    const container = document.querySelector('.container')
+    const swapy = createSwapy(container)
+    swapy.onSwap(({ data }) => {
+      localStorage.setItem('slotItem', JSON.stringify(data.object))
+    })
+  }, [])
+  return (
+    <div className="container">
+        <div className="slot a" data-swapy-slot="1">
+          {getItemById(slotItems['1'])}
+        </div>
+        <div className="second-row">
+          <div className="slot b" data-swapy-slot="2">
+            {getItemById(slotItems['2'])}
+          </div>
+          <div className="slot c" data-swapy-slot="3">
+            {getItemById(slotItems['3'])}
+          </div>
+        </div>
+        <div className="slot d" data-swapy-slot="4">
+          {getItemById(slotItems['4'])}
+        </div>
+      </div>
+  )
+}
+
+export default App
